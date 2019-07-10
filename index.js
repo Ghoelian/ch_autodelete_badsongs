cac10e03af55a9cb816e80db859e63f2c2bccc73
Julian Vos <post@julianvos.nl>
2019-07-10 23:31:59 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const isValid = require('is-valid-path')
+const rimraf = require('rimraf')
+const fs = require('fs')
+const args = process.argv.slice(2)
+const arg = args[0]
+
+if (isValid(arg)) {
+  fs.readFile(arg, 'utf8', (err, data) => {
+    if (err) throw err
+
+    const paths = data.split('\n')
+
+    for (let i = 0; i < paths.length; i++) {
+      if (paths[i] !== '\r' && !paths[i].includes('These folders')) {
+        rimraf(paths[i], () => {
+          console.log('Removed folder: ' + paths[i])
+        })
+      }
+    }
+  })
+}
